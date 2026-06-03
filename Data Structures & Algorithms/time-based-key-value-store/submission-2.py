44f2a8b14f39b3ddc0b41e9abcdc29fbe523904a
RealBMan <68237020+RealBMan@users.noreply.github.com>
2026-06-02 20:26:51 -0400

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,32 @@
+class TimeMap:
+
+    def __init__(self):
+        self.dict_val = {}
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+            if key in self.dict_val:
+                self.dict_val[key].append((timestamp,value))
+            else:
+                self.dict_val[key] = [(timestamp,value)]
+
+    def get(self, key: str, timestamp: int) -> str:
+            if key not in self.dict_val:
+                return ""
+            if len(self.dict_val[key]) == 0:
+                return ""
+            else:
+                l = 0
+                r = len(self.dict_val[key]) - 1
+                ans = ""
+                
+                while l <= r:
+                    mid = (l + r + 1) // 2
+                    if self.dict_val[key][mid][0] <= timestamp:
+                        ans = self.dict_val[key][mid][1]
+                        l = mid + 1
+                    else:
+                        r = mid - 1
+                return ans
+
+        
